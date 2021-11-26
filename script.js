b6ad4918984546ad4ea60ee17bce7b61dfda85ac
onlis <aleksei@onl.is>
2021-11-26 19:13:34 +0500

    hard exercise done

--- script.js
+++ script.js
@@ -18,4 +18,22 @@ for (; i >= 0; i--) {
     break;
   }
 }
-console.log(first_step);
+console.log(first_step); //произведение (умножение) цифр числа 266219
+
+let second_step;
+second_step = first_step ** 3;
+console.log(second_step); //результат возведен в степень 3
+
+let third_step;
+i = 0;
+while (rslt !== 0) {
+  i++;
+  rslt = Math.floor(second_step / 10 ** i);
+}
+i--;
+let steps = i - 2;
+for (; i > steps; i--) {
+  third_step = Math.floor(second_step / 10 ** i);
+  second_step = second_step - third_step * 10 ** i;
+  console.log(third_step); // вывод в консоль первых двух цифр
+}
