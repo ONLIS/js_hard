de94279f319eca994dec2062dcbfecab692189ca
onlis <aleksei@onl.is>
2021-11-29 12:29:35 +0500

    lesson03 hard done

--- script.js
+++ script.js
@@ -1,39 +1,56 @@
-let num = 266219;
-let i = 0;
-let rslt = 1;
-let first_step = 1;
-while (rslt !== 0) {
-  i++;
-  rslt = Math.floor(num / 10 ** i);
-}
-i--;
-for (; i >= 0; i--) {
-  rslt = num;
-  num = num - Math.floor(num / 10 ** i) * 10 ** i;
-  rslt = Math.floor((rslt - num) / 10 ** i);
-  if (rslt > 0) {
-    first_step *= rslt;
-  } else {
-    first_step = 0;
-    break;
-  }
-}
-console.log(first_step); //произведение (умножение) цифр числа 266219
+let lang = confirm("Ваш язык русский?") ? "ru" : "en";
+console.log(lang);
 
-let second_step;
-second_step = first_step ** 3;
-console.log(second_step); //результат возведен в степень 3
+let dowArray = {
+  ru: ["пн", "вт", "ср", "чт", "пт", "сб", "вс"],
+  en: ["mn", "tu", "wd", "th", "fr", "st", "sun"],
+};
+dowArray[lang].forEach((element) => {
+  console.log(element);
+});
 
-let third_step;
-i = 0;
-while (rslt !== 0) {
-  i++;
-  rslt = Math.floor(second_step / 10 ** i);
+if (lang == "ru") {
+  console.log("понедельник");
+  console.log("вторник");
+  console.log("среда");
+  console.log("четверг");
+  console.log("пятница");
+  console.log("суббота");
+  console.log("воскресенье");
+} else {
+  console.log("monday");
+  console.log("tuesday");
+  console.log("wednesday");
+  console.log("thursday");
+  console.log("friday");
+  console.log("saturday");
+  console.log("sunday");
 }
-i--;
-let steps = i - 2;
-for (; i > steps; i--) {
-  third_step = Math.floor(second_step / 10 ** i);
-  second_step = second_step - third_step * 10 ** i;
-  console.log(third_step); // вывод в консоль первых двух цифр
+
+switch (lang) {
+  case "en":
+    console.log("monday");
+    console.log("tuesday");
+    console.log("wednesday");
+    console.log("thursday");
+    console.log("friday");
+    console.log("saturday");
+    console.log("sunday");
+  case "ru":
+    console.log("понедельник");
+    console.log("вторник");
+    console.log("среда");
+    console.log("четверг");
+    console.log("пятница");
+    console.log("суббота");
+    console.log("воскресенье");
 }
+
+let namePerson = prompt("What is your name?");
+console.log(
+  namePerson == "Артем"
+    ? "Директор"
+    : namePerson == "Александр"
+    ? "профессор"
+    : "студент"
+);
