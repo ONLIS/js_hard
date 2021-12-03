bf2555015b6f3a739884e9ca3827a9758cde4237
onlis <aleksei@onl.is>
2021-12-03 15:52:13 +0500

    lesson07 hard done

--- script.js
+++ script.js
@@ -1,49 +1,27 @@
 "use strict";
 
-const limit = 100;
-let nmbArray = ["234", "33", "57", "54829", "29", "773", "4342"];
-let resultArray1 = [];
-let resultArray2 = [];
+const htmlElm = document.querySelector(".day-of-week");
+const today = new Date();
+const dayOfWeek = today.getDay() == 0 ? 7 : today.getDay();
+let daysArray = [
+  "monday",
+  "tuesday",
+  "wednesday",
+  "thursday",
+  "friday",
+  "saturday",
+  "sunday",
+];
 
-function function1(testArray) {
-  testArray.forEach((elm) => {
-    if (elm[0] == "2" || elm[0] == "4") {
-      resultArray1.push(elm);
-    }
-  });
-}
-function function2(limitNumber) {
-  let testNumber = 2;
-  let dividerArr;
-  let message;
-  while (testNumber <= limitNumber) {
-    dividerArr = [];
-    message = "";
-    for (let i = 1; i <= testNumber; i++) {
-      if (testNumber % i == 0) {
-        dividerArr.push(i);
-      }
-    }
-    if (dividerArr.length == 2) {
-      message =
-        "Простое число: " +
-        testNumber +
-        ". Делители этого числа: " +
-        dividerArr[0] +
-        " и " +
-        dividerArr[1];
-      resultArray2.push(message);
-    }
-    testNumber++;
+daysArray.forEach(function (currentValue, index) {
+  let strng = "";
+  let styles = "day-of-week__item";
+  if (index == dayOfWeek - 1) {
+    styles += " day-of-week__item--bold";
   }
-}
-
-function1(nmbArray);
-function2(limit);
-
-resultArray1.forEach((elm) => {
-  console.log(elm);
-});
-resultArray2.forEach((elm) => {
-  console.log(elm);
+  if (index == 5 || index == 6) {
+    styles += " day-of-week__item--italic";
+  }
+  strng += "<div class='" + styles + "'>" + currentValue + "</div>";
+  htmlElm.innerHTML += strng;
 });
